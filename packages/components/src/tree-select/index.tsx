0896944378a7aefc32cd818270e39e19fcb12bb4
Yi Yunwan <yyw9603@163.com>
2022-11-27 21:26:29 +0800

    feat: add TreeSelect

--- packages/components/src/tree-select/index.tsx
+++ packages/components/src/tree-select/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { connect, mapReadPretty, mapProps } from '@formily/react'
-import { TreeSelect as AntdTreeSelect } from 'antd'
-import { PreviewText } from '../preview-text'
 import { LoadingOutlined } from '@ant-design/icons'
+import { connect, mapProps, mapReadPretty } from '@formily/react'
+import { TreeSelect as AntdTreeSelect } from 'antd'
+import React from 'react'
+import { PreviewText } from '../preview-text'
+
 export const TreeSelect = connect(
   AntdTreeSelect,
   mapProps(

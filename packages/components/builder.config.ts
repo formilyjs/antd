6f87b9e7b94d1a4305582d41a5f1f5a8f452dca9
yiyunwan <yyw9603@163.com>
2022-12-19 11:23:53 +0800

    ci: use codecov-action@v3

--- packages/components/builder.config.ts
+++ packages/components/builder.config.ts
@@ -4,4 +4,8 @@ export const BuilderConfig: IBuilderConfig = {
   targetLibName: 'antd',
   targetLibCjsDir: 'lib',
   targetLibEsDir: 'es',
+  externals: {
+    antd: 'antd',
+    dayjs: 'dayjs',
+  },
 }
